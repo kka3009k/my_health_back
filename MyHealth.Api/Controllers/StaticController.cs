467c1983e1694ad13327e7376d47252f78a5366b
kkargin <kkargin@bf.kg>
2023-08-30 15:02:58 +0600

    Добавил Пользовательское соглашение

---
+++ MyHealth.Api/Controllers/StaticController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyHealth.Data;
+using MyHealth.Data.Dto;
+using MyHealth.Data.Entities;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MyHealth.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StaticController : ControllerBase
+    {
+        private readonly ILogger<StaticController> _logger;
+        private readonly MyDbContext _db;
+
+        public StaticController(ILogger<StaticController> logger, MyDbContext pDb)
+        {
+            _logger = logger;
+            _db = pDb;
+        }
+
+        /// <summary>
+        /// Пользовательское соглашение
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("terms_of_use")]
+        public async Task<ContentResult> TermOfUse()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "static", "TermsOfUse.html");
+            var html = await new StreamReader(path).ReadToEndAsync();
+            return Content(html, "text/html");
+        }
+    }
+}
