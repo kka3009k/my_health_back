7f10a4924642fd4028a4f51c44997b44cc3d8b38
kka3009k <konstantin.kargin.su@gmail.com>
2023-08-31 19:53:03 +0600

    add status check

---
+++ MyHealth.Api/Controllers/StatusController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyHealth.Api.Service;
+using MyHealth.Data;
+using MyHealth.Data.Dto;
+using MyHealth.Data.Entities;
+using System.ComponentModel;
+using System.Net;
+using System.Reflection;
+
+namespace MyHealth.Api.Controllers
+{
+    [ApiController]
+    [Route("status_check")]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger<StatusController> _logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Получить соответствующий статус
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{code}")]
+        [HttpPost("{code}")]
+        public IActionResult GetStatusResult(int code)
+        {
+            return StatusCode(code, $"request status: {code}");
+        }
+
+        
+    }
+}
