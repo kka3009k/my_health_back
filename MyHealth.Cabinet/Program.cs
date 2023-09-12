2768050ad010ebf7cf7b3911ecf58d0bdd18d606
Marat Temir <temirbaev.marat@gmail.com>
2023-09-12 19:58:25 +0600

    Страничка Регистрации

--- MyHealth.Cabinet/Program.cs
+++ MyHealth.Cabinet/Program.cs
@@ -13,7 +13,11 @@ namespace MyHealth.Cabinet
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddScoped(sp =>
+            new HttpClient
+            {
+                 BaseAddress = new Uri("https://localhost:7001/")
+            });
             await builder.Build().RunAsync();
         }
     }
