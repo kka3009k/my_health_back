b733add6d4d0f391f2b70f88440df3f845b98bd8
Marat Temir <temirbaev.marat@gmail.com>
2023-09-12 17:32:36 +0600

    Кабинет

---
+++ MyHealth.Api/Controllers/Cabinet/DoctorRegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyHealth.Api.Controllers.Cabinet
+{
+    public class DoctorRegistrationController : Controller
+    {
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
