57a5c5ee38ebc4e86e0c34cfe07073141598ef3f
Temirbaev Marat <temirbaev.marat@gmail.com>
2023-09-18 22:55:59 +0600

    Регистрация

---
+++ MyHealth.Cabinet/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace MyHealth.Cabinet.Pages
+{
+    public partial class Index
+    { 
+
+
+        [Inject] private HttpClient httpClient { get; set; }
+        public async void UserRegistration()
+        {
+            
+        }
+    }
+}
